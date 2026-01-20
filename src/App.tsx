6fabd946613e1978e3636c36d761ece5a8d6a75e
Shob3r <andrewpinter23@gmail.com>
2026-01-19 16:20:26 -0800

    project start

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Titlebar from "./Components/Titlebar";
+
+function App() {
+  return (
+    <main class="h-screen w-screen bg-white">
+      <Titlebar/>
+      <h1>It's Taurin' Time</h1>
+    </main>
+  );
+}
+
+export default App;
---
+++ src/Components/Titlebar.tsx
@@ -0,0 +1,10 @@
+
+export default function Titlebar() {
+    return(
+        <>
+            <div data-tauri-drag-region class="w-full h-auto px-3 py-1.5 bg-gray-800 text-white">
+                asdsad
+            </div>
+        </>
+    )
+}
