38fa64fd203e61497cccef3bf90ecb464a5d8e7e
ChakornK <chakornk2007@gmail.com>
2026-01-21 21:12:37 -0800

    fix

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -14,7 +14,6 @@ pub fn run() {
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_http::init())
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
