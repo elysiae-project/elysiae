b7fbf510a54162a90ba7f2fb00dc5f500460a2fc
Shob3r <andrewpinter23@gmail.com>
2026-01-26 16:07:29 -0800

    feat: finalize github action

--- generate-build.sh
+++ generate-build.sh
@@ -8,12 +8,20 @@
 # org.freedesktop.Sdk.Extension.node20//24.08 (flatpak)
 # org.freedesktop.Sdk.Extension.rust-stable//24.08 (flatpak)
 
-export RUSTFLAGS="-C target-cpu=native" 
+# Extra optimization flags
+export RUSTFLAGS="-C target-cpu=native"
 
+# Run yarn before entering build environment to install/update packages
+yarn
+
+# Start flatpak-builder build
 flatpak-builder --repo=repo --force-clean build net.shob3r.yoohoo.yml
+
+# Convert flatpak-builder build output
 flatpak build-bundle repo net.shob3r.yoohoo.flatpak net.shob3r.yoohoo
+
+# Clean up build directories
 rm -rf repo
 rm -rf .flatpak-builder
 rm -rf build
-
 unset RUSTFLAGS
