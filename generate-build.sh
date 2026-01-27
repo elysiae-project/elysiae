ad668578bc6e6fa9133eccbf8b05b00109668e8d
Shob3r <andrewpinter23@gmail.com>
2026-01-27 02:49:53 -0800

    feat: improved build generator script, get build action to use script

--- generate-build.sh
+++ generate-build.sh
@@ -1,27 +1,46 @@
-# This script is a work-in-progress and is the intended way to build for release.
-# I plan on adding automatic dependency installation (both from os package manager and the flatpak SDKs) in the near future
-# For now, install:
-# flatpak (os pkg manager)
-# flatpak-builder (os pkg manager)
-# org.gnome.Platform//48 (flatpak)
-# org.gnome.Sdk//48 (flatpak)
-# org.freedesktop.Sdk.Extension.node20//24.08 (flatpak)
-# org.freedesktop.Sdk.Extension.rust-stable//24.08 (flatpak)
+set -e
 
-# Extra optimization flags
-export RUSTFLAGS="-C target-cpu=native"
+cd "$(dirname "$0")"
 
-# Run yarn before entering build environment to install/update packages
+echo "(1/7) Checking for system dependencies"
+for command in flatpak flatpak-builder node npm; do 
+    if ! $command --version >/dev/null 2>&1
+    then
+        echo "$command does not appear to be installed on your system"
+        echo "Please install all required build dependencies before running again"
+        exit 1
+    fi
+done
+
+echo "(2/7) Checking for & installing missing flatpak build dependencies"
+for build_dep in org.gnome.Platform//48 org.gnome.Sdk//48 org.freedesktop.Sdk.Extension.node24//25.08 org.freedesktop.Sdk.Extension.rust-stable//25.08; do
+    if ! $build_dep --version >/dev/null 2>&1
+    then
+        echo "Installing build dependency $build_dep"
+        flatpak install -y flathub $build_dep
+    fi
+done
+
+echo "(3/7) Preparing frontend dependency install"
+if ! corepack --version >/dev/null 2>&1
+then
+    echo "Installing corepack"
+    npm i -g corepack
+    corepack enable
+fi
+
+echo "(4/7) Installing frontend dependencies"
 yarn
 
-# Start flatpak-builder build
+echo "(5/7) Building flatpak app"
 flatpak-builder --repo=repo --force-clean build net.shob3r.yoohoo.yml
 
-# Convert flatpak-builder build output
+echo "(6/7) Bundling flatpak app"
 flatpak build-bundle repo net.shob3r.yoohoo.flatpak net.shob3r.yoohoo
 
-# Clean up build directories
-rm -rf repo
-rm -rf .flatpak-builder
-rm -rf build
-unset RUSTFLAGS
+echo "(7/7) Cleanup"
+rm -rf repo & >/dev/null
+rm -rf .flatpak-builder & >/dev/null
+rm -rf build & >/dev/null
+
+echo "Done! .flatpak file located in $PWD/net.shob3r.yoohoo.flatpak"
