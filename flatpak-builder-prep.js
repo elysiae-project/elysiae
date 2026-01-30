24a27310acca4255bb94fd303f47e42f40eb3beb
ChakornK <chakornk2007@gmail.com>
2026-01-29 23:11:37 -0800

    fix: resolve npm and cargo dependencies from offline files properly

--- flatpak-builder-prep.js
+++
@@ -1,5 +0,0 @@
-import fs from "node:fs";
-const file = "package.json";
-const json = JSON.parse(fs.readFileSync(file));
-delete json.packageManager;
-fs.writeFileSync(file, JSON.stringify(json, null, 4));
