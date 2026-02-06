a4c31d31dbaf80e8db80a91eae10a37e352db87b
Shob3r <andrewpinter23@gmail.com>
2026-02-06 01:57:14 -0800

    feat: modal also, add branding colours for app and add svg icon

--- src/components/ys/Modal.tsx
+++ src/components/ys/Modal.tsx
@@ -1,14 +1,28 @@
 import { Minimize2 } from "lucide-preact";
+import { MutableRef, useEffect, useRef } from "preact/hooks";
 
 export default function YsModal({
 	children,
 	title,
+	open,
 }: {
 	children: any;
 	title: string;
+	open: boolean;
 }) {
+	const dialog: MutableRef<null | HTMLDialogElement> = useRef(null);
+
+	useEffect(() => {
+		if (open) {
+			dialog.current?.close();
+		} else dialog.current?.showModal();
+	}, [open]);
+
 	return (
-		<dialog class="w-auto min-w-45 h-auto min-h-30 overflow-y-scroll bg-[#3b4354] rounded-lg px-5 py-3">
+		<dialog
+			ref={dialog}
+			class="w-auto min-w-45 h-auto min-h-30 overflow-y-scroll bg-[#3b4354] rounded-lg px-5 py-3"
+		>
 			<div class="flex flex-row justify-between items-center">
 				<h2 class="text-[#d2bc8d]">{title}</h2>
 				<Minimize2 color="#d2bc8d" />
