887db62f9cfe7f9d190d64294e3d8ec0adf5188e
Shob3r <andrewpinter23@gmail.com>
2026-02-20 01:30:48 -0800

    complete modularizing wine environment creator

--- src/util/WineTools.ts
+++ src/util/WineTools.ts
@@ -1,14 +1,19 @@
 import { downloadFile, getGithubInfo } from "./WebUtils";
-import { getActiveGameCode } from "./AppFunctions";
-import { exists, remove } from "@tauri-apps/plugin-fs";
+import { exists, mkdir, remove } from "@tauri-apps/plugin-fs";
 import { join, resourceDir } from "@tauri-apps/api/path";
 import { extractFile, getAllDirs, moveDirItems } from "./FileUtils";
 import { Command } from "@tauri-apps/plugin-shell";
 
-export const createWineEnvironment = async (firstInstall: boolean = true) => {
+export const createWineEnvironment = async () => {
+	// Wine prefix creation should be in this order:
+	// 1. Download and extract wine
+	// 2. Download winetricks
+	// 3. Use winetricks to install C++ redist and dxvk, which also creates a wineprefix automatically
+	// 4. Install vkd3d-proton for futureproofing against future games that will use dx12.
+	// 	  This install process relies on the wineprefix created in step 3
 	await updateWine();
 	await updateWinetricks();
-	if (firstInstall) await updateWinetricksModules();
+	await updateWinetricksModules();
 	await updateVkd3d();
 };
 
@@ -17,6 +22,7 @@ export const updateWine = async () => {
 	const downloadLocation = await join(appDir, "wine.tar.xz");
 	const extractLocation = await join(appDir, "wine-temp");
 	const finalLocation = await join(appDir, "wine");
+	await mkdir(finalLocation);
 
 	const repoInfo = await getGithubInfo(
 		"https://api.github.com/repos/NelloKudo/spritz-wine-aur/releases/latest",
@@ -27,7 +33,11 @@ export const updateWine = async () => {
 
 	const folder = (await getAllDirs(extractLocation))[0];
 	await moveDirItems(folder, finalLocation);
+
 	await remove(downloadLocation);
+	await remove(extractLocation, {
+		recursive: true,
+	});
 };
 
 export const updateWinetricks = async () => {
@@ -50,10 +60,16 @@ export const updateVkd3d = async () => {
 	const extractLocation = await join(appDir, "vkd3d-proton-temp");
 	const wineDir = await join(appDir, "wine");
 
+	const repoInfo = await getGithubInfo(
+		"https://api.github.com/repos/HansKristian-Work/vkd3d-proton/releases/latest",
+	);
+
+	await downloadFile(repoInfo.downloadURL, downloadLocation);
+	await extractFile(downloadLocation, extractLocation);
+
 	if (!(await exists(wineDir))) {
-		await updateWine();
-		await updateWinetricks(); // Generates the wineprefix
-		await updateWinetricksModules();
+		await createWineEnvironment();
+		return;
 	}
 
 	// Not using rename() because this is a lot cleaner
@@ -75,15 +91,15 @@ export const updateVkd3d = async () => {
 	]);
 
 	await remove(downloadLocation);
-	await remove(extractLocation);
+	await remove(extractLocation, {
+		recursive: true,
+	});
 };
 
 export const updateWinetricksModules = async () => {
+	// Trying to install a few different redists to ensure that older games will still run properly.
+	// Not sure if just vcrun2022/vcrun2026 will be able to do this
 	await winetricksCommand(["vcrun2019 vcrun2022 vcrun2026 dxvk"]);
-};
-
-const launchGame = async () => {
-	const game = getActiveGameCode();
 };
 
 const wineCommand = async (commands: string[]) => {
