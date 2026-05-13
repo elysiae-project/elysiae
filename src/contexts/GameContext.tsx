0c12dec5c0a861478e3ff4055d0d02b3ffe56371
Shob3r <andrewpinter23@gmail.com>
2026-05-13 02:21:48 -0700

    feat: add last selected game tab loading

--- src/contexts/GameContext.tsx
+++ src/contexts/GameContext.tsx
@@ -1,6 +1,9 @@
 import { createContext, ComponentChildren } from "preact";
-import { useState } from "preact/hooks";
+import { useEffect, useState } from "preact/hooks";
 import { Variants } from "../types";
+import { getOption, setOption } from "../util/Settings";
+import { getActiveGameCode, getVariantFromCode } from "../util/AppFunctions";
+import { warn } from "@tauri-apps/plugin-log";
 
 interface GameContextType {
 	game: Variants;
@@ -14,6 +17,24 @@ export const GameContext = createContext<GameContextType>({
 
 export const GameProvider = ({ children }: { children: ComponentChildren }) => {
 	const [game, setGame] = useState<Variants>(Variants.HKRPG);
+	useEffect(() => {
+		(async () => {
+			const lastSelectedGame = getVariantFromCode(await getOption<string>("selectedGame"));
+			if(lastSelectedGame) {
+				setGame(lastSelectedGame);
+			}
+			else {
+				warn("GameProvider: selectedGame option is missing, null, or empty");
+			}
+		})()
+	}, []);
+
+	useEffect(() => {
+		(async () => {
+			const gameCode = getActiveGameCode(game);
+			await setOption("selectedGame", gameCode);
+		})();
+	}, [game]);
 
 	return (
 		<GameContext.Provider value={{ game, setGame }}>
