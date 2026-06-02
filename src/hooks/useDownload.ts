e13f84c571143b8b042ce2b6fa41990c51e66738
Shob3r <andrewpinter23@gmail.com>
2026-06-02 12:21:35 -0700

    fix(useDownload): throw error when declared outside of DownloadProvider

--- src/hooks/useDownload.ts
+++ src/hooks/useDownload.ts
@@ -1,4 +1,10 @@
 import { useContext } from "preact/hooks";
 import { DownloadContext } from "../contexts/DownloadContext";
 
-export const useDownload = () => useContext(DownloadContext);
+export const useDownload = () => {
+	const context = useContext(DownloadContext);
+	if (!context) {
+		throw new Error("useDownload must be used within a DownloadProvider");
+	}
+	return context;
+};
