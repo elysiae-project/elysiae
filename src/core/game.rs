b901565b77841463741dedd970cb6281e86e2ae4
Shob3r <62476902+Shob3r@users.noreply.github.com>
2026-09-07 21:58:23 -0700

    feat: add hyg and abc game code support

--- src/core/game.rs
+++ src/core/game.rs
@@ -2,12 +2,16 @@ use std::path::PathBuf;
 
 use anyhow::{Ok, bail};
 
+use crate::core::game::Game::Abc;
+
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum Game {
     Bh3,
     Hk4e,
     Hkrpg,
     Nap,
+    Abc,
+    Hyg,
 }
 
 impl TryFrom<&str> for Game {
@@ -19,6 +23,8 @@ impl TryFrom<&str> for Game {
             "\x68\x6b\x34\x65" => Ok(Self::Hk4e),
             "\x68\x6b\x72\x70\x67" => Ok(Self::Hkrpg),
             "\x6e\x61\x70" => Ok(Self::Nap),
+            "\x61\x62\x63" => Ok(Self::Abc),
+            "\x68\x79\x67" => Ok(Self::Hyg),
             _ => bail!("Unsupported game code: {code:?}"),
         }
     }
@@ -31,6 +37,8 @@ impl Game {
             Self::Hk4e => "\x68\x6b\x34\x65",
             Self::Hkrpg => "\x68\x6b\x72\x70\x67",
             Self::Nap => "\x6e\x61\x70",
+            Self::Abc => "\x61\x62\x63",
+            Self::Hyg => "\x68\x79\x67",
         }
     }
 
@@ -40,6 +48,8 @@ impl Game {
             Self::Hk4e => "\x47\x65\x6e\x73\x68\x69\x6e\x20\x49\x6d\x70\x61\x63\x74",
             Self::Hkrpg => "\x48\x6f\x6e\x6b\x61\x69\x3a\x20\x53\x74\x61\x72\x20\x52\x61\x69\x6c",
             Self::Nap => "\x5a\x65\x6e\x6c\x65\x73\x73\x20\x5a\x6f\x6e\x65\x20\x5a\x65\x72\x6f",
+            Self::Abc => "\x48\x6f\x6e\x6b\x61\x69\x20\x4e\x65\x78\x75\x73\x20\x41\x6e\x69\x6d\x61",
+            Self::Hyg => "\x50\x65\x74\x69\x74\x20\x50\x6c\x61\x6e\x65\x74",
         }
     }
 
@@ -51,6 +61,9 @@ impl Game {
             Self::Nap => {
                 "\x5a\x65\x6e\x6c\x65\x73\x73\x5a\x6f\x6e\x65\x5a\x65\x72\x6f\x2e\x65\x78\x65"
             }
+            // FIXME: I won't know the executable names of these two until the sophon endpoints for the games release. add executable names after the fact
+            Self::Abc => "",
+            Self::Hyg => "",
         }
     }
 
